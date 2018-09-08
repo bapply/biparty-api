b9dfc3509341674d8c9abbe77b199cedc071ea9b
Erfan Besharat <erbesharat@gmail.com>
2018-09-08 16:39:56 +0430

    Add tests for login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use http\Env\Request;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $user = User::create();
         $token = JWTAuth::fromUser($user);
