9fd098f74aea49f753383385c66b48f0f77b933e
Erfan Besharat <erbesharat@gmail.com>
2018-09-16 16:22:48 +0430

    Read app port from env variables

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/bapply/biparty/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if godotenv.Load() != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
 		v1.GET("/users", controllers.CreateUser)
 	}
-	router.Run()
+	router.Run(os.Getenv("BIPARTY_PORT"))
 }
